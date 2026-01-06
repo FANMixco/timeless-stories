b7280053659021c4fd989a540b274906d6f3bbef
Federico Navarrete <fanmixco@gmail.com>
2026-01-06 18:58:04 +0100

    Spanish edition corrections

--- js/translations.js
+++ js/translations.js
@@ -1,59 +1,77 @@
 let nLang = (navigator.languages
-    ? navigator.languages[0]
-    : (navigator.language || navigator.userLanguage)).split('-')[0];
+	? navigator.languages[0]
+	: (navigator.language || navigator.userLanguage)).split('-')[0];
 
 let supportedLang = ['en', 'es', 'zh'];
 let translations;
 const lang = supportedLang.includes(nLang) ? nLang : 'en';
 
 fetchData(`js/i18n/lang-${lang}.min.json`)
-    .then((data) => {
-        translations = data.translations;
+	.then((data) => {
+		translations = data.translations;
 
-        // Apply translations to DOM
-        document.querySelectorAll('[data-translation]').forEach(item => {
-            item.innerHTML = translations[item.dataset.translation];
-        });
+		// Apply translations to DOM
+		document.querySelectorAll('[data-translation]').forEach(item => {
+			item.innerHTML = translations[item.dataset.translation];
+		});
 
-        // Update Amazon and Books2Read links
-        const amazonBtn = document.getElementById("price0");
-        if (amazonBtn) {
-            amazonBtn.href = (lang === 'es')
-                ? "https://a.co/d/codBr2F"
-                : "https://a.co/d/0jrpI9T";
-        }
+		const tEdition10 = document.getElementById("tEdition10");
+		const tEdition4 = document.getElementById("tEdition4");
+		//const tEdition8 = document.getElementById("tEdition8");
+		//const tEdition12 = document.getElementById("tEdition12");
+		const tEdition2 = document.getElementById("tEdition2");
+		const tEdition6 = document.getElementById("tEdition6");
 
-        const bookAnchor = document.querySelector("#div-book a");
-        if (bookAnchor) {
-            bookAnchor.href = (lang === 'es')
-                ? "https://books2read.com/u/4NEzj9"
-                : "https://books2read.com/u/3nXNNR";
-        }
-        document.title = (lang === 'es')
-            ? 'Historias Eternas de El Salvador | Folclore, Leyendas y Cuentos'
-            : 'Timeless Stories of El Salvador | Folklore, Legends & Fairy tales';
+		if (lang === 'es') {
+			tEdition10.href = 'https://adbl.co/4qExKCP';
+			tEdition4.href = 'https://a.co/d/cVQ0B39';
+			document.getElementById('dEdition8').style.display ='none';
+			document.getElementById('dEdition12').style.display ='none';
+			tEdition2.href = 'https://a.co/d/e4W03f0';
+			tEdition6.href = 'https://a.co/d/6ycRDq4';
+		}
 
 
-        // Update image if Spanish version is needed
-        const picture = document.querySelector("#div-book picture");
-        if (picture && lang === 'es') {
-            picture.innerHTML = `
-            <source srcset="img/cover-colorized-v2-es.webp" type="image/webp">
-            <source srcset="img/cover-colorized-v2-es.jpg" type="image/jpeg">
-            <img style="width: 100%;" alt="Historias Eternas de El Salvador V2"
-                src="img/cover-colorized-v2-es.jpg" />
+		//to be re-enabled
+		// Update Amazon and Books2Read links
+		/*const amazonBtn = document.getElementById("price0");
+		if (amazonBtn) {
+			amazonBtn.href = (lang === 'es')
+				? "https://a.co/d/codBr2F"
+				: "https://a.co/d/0jrpI9T";
+		}
+
+		const bookAnchor = document.querySelector("#div-book a");
+		if (bookAnchor) {
+			bookAnchor.href = (lang === 'es')
+				? "https://books2read.com/u/4NEzj9"
+				: "https://books2read.com/u/3nXNNR";
+		}*/
+		document.title = (lang === 'es')
+			? 'Historias Eternas de El Salvador | Folclore, Leyendas y Cuentos'
+			: 'Timeless Stories of El Salvador | Folklore, Legends & Fairy tales';
+
+
+		// Update image if Spanish version is needed
+		const picture = document.querySelector("#div-book picture");
+		if (picture && lang === 'es') {
+			picture.innerHTML = `
+            <source srcset="img/cover-colorized-v2-sm-es.webp" type="image/webp">
+            <source srcset="img/cover-colorized-v2-sm-es.jpg" type="image/jpeg">
+            <img style="width: 100%;" alt="Historias Eternas de El Salvador — Espejos Españoles"
+                src="img/cover-colorized-v2-sm-es.jpg" />
         `;
-        }
+		}
 
-        if (lang === 'es') {
-            const btnEditor = document.getElementById('btnEditor');
-            btnEditor.style.setProperty("display", "none", "important");
+		if (lang === 'es') {
+			const btnEditor = document.getElementById('btnEditor');
+			btnEditor.style.setProperty("display", "none", "important");
 
-            const desktopCarouselInner = document.querySelector('#multiItemCarousel .carousel-inner');
-            const mobileCarouselInner = document.querySelector('#galleryCarousel .carousel-inner');
+			const desktopCarouselInner = document.querySelector('#multiItemCarousel .carousel-inner');
+			const mobileCarouselInner = document.querySelector('#galleryCarousel .carousel-inner');
 
-            if (desktopCarouselInner) {
-                desktopCarouselInner.innerHTML = `
+			if (desktopCarouselInner) {
+				desktopCarouselInner.innerHTML = `
                 <div class="carousel-item active">
                   <div class="row">
                     <div class="col-12 col-md-6 col-lg-4">
@@ -94,10 +112,10 @@ fetchData(`js/i18n/lang-${lang}.min.json`)
                     </div>
                   </div>
                 </div>`;
-            }
+			}
 
-            if (mobileCarouselInner) {
-                mobileCarouselInner.innerHTML = `
+			if (mobileCarouselInner) {
+				mobileCarouselInner.innerHTML = `
                 <div class="carousel-item active">
                   <div class="single-price no-padding">
                     <div class="price-top">
@@ -134,17 +152,17 @@ fetchData(`js/i18n/lang-${lang}.min.json`)
                     </div>
                   </div>
                 </div>`;
-            }
+			}
 
-            // Re-run translation injection on the newly inserted elements
-            document.querySelectorAll('[data-translation]').forEach(item => {
-                const key = item.dataset.translation;
-                if (translations[key]) {
-                    item.innerHTML = translations[key];
-                }
-            });
-        }
-    })
-    .catch((e) => {
-        console.error(e);
-    });
+			// Re-run translation injection on the newly inserted elements
+			document.querySelectorAll('[data-translation]').forEach(item => {
+				const key = item.dataset.translation;
+				if (translations[key]) {
+					item.innerHTML = translations[key];
+				}
+			});
+		}
+	})
+	.catch((e) => {
+		console.error(e);
+	});
