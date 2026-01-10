79d6cde746b1b8abe7847b0551f72058f82bb650
Federico Navarrete <fanmixco@gmail.com>
2026-01-10 19:24:03 +0100

    Minor UI corrections

--- js/translations.js
+++ js/translations.js
@@ -1,77 +1,76 @@
 let nLang = (navigator.languages
-	? navigator.languages[0]
-	: (navigator.language || navigator.userLanguage)).split('-')[0];
+    ? navigator.languages[0]
+    : (navigator.language || navigator.userLanguage)).split('-')[0];
 
 let supportedLang = ['en', 'es', 'zh', 'fr'];
 let translations;
 const lang = supportedLang.includes(nLang) ? nLang : 'en';
 
 fetchData(`js/i18n/lang-${lang}.min.json`)
-	.then((data) => {
-		translations = data.translations;
+    .then((data) => {
+        console.log('begin');
+        translations = data.translations;
 
-		// Apply translations to DOM
-		document.querySelectorAll('[data-translation]').forEach(item => {
-			item.innerHTML = translations[item.dataset.translation];
-		});
-
-		const tEdition10 = document.getElementById("tEdition10");
-		const tEdition4 = document.getElementById("tEdition4");
-		//const tEdition8 = document.getElementById("tEdition8");
-		//const tEdition12 = document.getElementById("tEdition12");
-		const tEdition2 = document.getElementById("tEdition2");
-		const tEdition6 = document.getElementById("tEdition6");
-
-		if (lang === 'es') {
-			tEdition10.href = 'https://adbl.co/4qExKCP';
-			tEdition4.href = 'https://a.co/d/cVQ0B39';
-			document.getElementById('dEdition8').style.display ='none';
-			document.getElementById('dEdition12').style.display ='none';
-			tEdition2.href = 'https://a.co/d/e4W03f0';
-			tEdition6.href = 'https://a.co/d/6ycRDq4';
-		}
+        // Apply translations to DOM
+        document.querySelectorAll('[data-translation]').forEach(item => {
+            item.innerHTML = translations[item.dataset.translation];
+        });
 
 
-		//to be re-enabled
-		// Update Amazon and Books2Read links
-		/*const amazonBtn = document.getElementById("price0");
-		if (amazonBtn) {
-			amazonBtn.href = (lang === 'es')
-				? "https://a.co/d/codBr2F"
-				: "https://a.co/d/0jrpI9T";
-		}
+        //to be re-enabled
+        // Update Amazon and Books2Read links
+        /*const amazonBtn = document.getElementById("price0");
+        if (amazonBtn) {
+          amazonBtn.href = (lang === 'es')
+            ? "https://a.co/d/codBr2F"
+            : "https://a.co/d/0jrpI9T";
+        }
+    
+        const bookAnchor = document.querySelector("#div-book a");
+        if (bookAnchor) {
+          bookAnchor.href = (lang === 'es')
+            ? "https://books2read.com/u/4NEzj9"
+            : "https://books2read.com/u/3nXNNR";
+        }*/
 
-		const bookAnchor = document.querySelector("#div-book a");
-		if (bookAnchor) {
-			bookAnchor.href = (lang === 'es')
-				? "https://books2read.com/u/4NEzj9"
-				: "https://books2read.com/u/3nXNNR";
-		}*/
-		document.title = (lang === 'es')
-			? 'Historias Eternas de El Salvador | Folclore, Leyendas y Cuentos'
-			: 'Timeless Stories of El Salvador | Folklore, Legends & Fairy tales';
+        document.title = translations.title;
 
+        if (lang === 'es') {
+            // Update image if Spanish version is needed
+            //const picture = document.querySelector("#div-book picture");
+            //if (picture && lang === 'es') {
+            //}
 
-		// Update image if Spanish version is needed
-		const picture = document.querySelector("#div-book picture");
-		if (picture && lang === 'es') {
-			picture.innerHTML = `
-            <source srcset="img/cover-colorized-v2-sm-es.webp" type="image/webp">
-            <source srcset="img/cover-colorized-v2-sm-es.jpg" type="image/jpeg">
-            <img style="width: 100%;" alt="Historias Eternas de El Salvador — Espejos Españoles"
-                src="img/cover-colorized-v2-sm-es.jpg" />
-        `;
-		}
+            const tEdition10 = document.getElementById("tEdition10");
+            const tEdition4 = document.getElementById("tEdition4");
+            //const tEdition8 = document.getElementById("tEdition8");
+            //const tEdition12 = document.getElementById("tEdition12");
+            const tEdition2 = document.getElementById("tEdition2");
+            const tEdition6 = document.getElementById("tEdition6");
 
-		if (lang === 'es') {
-			const btnEditor = document.getElementById('btnEditor');
-			btnEditor.style.setProperty("display", "none", "important");
+            document.querySelector("#div-book picture").innerHTML = `
+                <source srcset="img/cover-colorized-v2-sm-es.webp" type="image/webp">
+                <source srcset="img/cover-colorized-v2-sm-es.jpg" type="image/jpeg">
+                <img style="width: 100%;" alt="Historias Eternas de El Salvador — Espejos Españoles"
+                    src="img/cover-colorized-v2-sm-es.jpg" />
+            `;
 
-			const desktopCarouselInner = document.querySelector('#multiItemCarousel .carousel-inner');
-			const mobileCarouselInner = document.querySelector('#galleryCarousel .carousel-inner');
+            tEdition10.href = 'https://adbl.co/4qExKCP';
+            tEdition4.href = 'https://a.co/d/cVQ0B39';
+            document.getElementById('dEdition8').style.display = 'none';
+            document.getElementById('dEdition12').style.display = 'none';
+            tEdition2.href = 'https://a.co/d/e4W03f0';
+            tEdition6.href = 'https://a.co/d/6ycRDq4';
 
-			if (desktopCarouselInner) {
-				desktopCarouselInner.innerHTML = `
+            //const btnEditor = document.getElementById('btnEditor');
+            document.getElementById('btnEditor').style.setProperty("display", "none", "important");
+        }
+
+        const desktopCarouselInner = document.querySelector('#multiItemCarousel .carousel-inner');
+        const mobileCarouselInner = document.querySelector('#galleryCarousel .carousel-inner');
+
+        if (desktopCarouselInner) {
+            desktopCarouselInner.innerHTML = `
                 <div class="carousel-item active">
                   <div class="row">
                     <div class="col-12 col-md-6 col-lg-4">
@@ -112,10 +111,10 @@ fetchData(`js/i18n/lang-${lang}.min.json`)
                     </div>
                   </div>
                 </div>`;
-			}
+        }
 
-			if (mobileCarouselInner) {
-				mobileCarouselInner.innerHTML = `
+        if (mobileCarouselInner) {
+            mobileCarouselInner.innerHTML = `
                 <div class="carousel-item active">
                   <div class="single-price no-padding">
                     <div class="price-top">
@@ -152,17 +151,38 @@ fetchData(`js/i18n/lang-${lang}.min.json`)
                     </div>
                   </div>
                 </div>`;
-			}
+        }
 
-			// Re-run translation injection on the newly inserted elements
-			document.querySelectorAll('[data-translation]').forEach(item => {
-				const key = item.dataset.translation;
-				if (translations[key]) {
-					item.innerHTML = translations[key];
-				}
-			});
-		}
-	})
-	.catch((e) => {
-		console.error(e);
-	});
+        // Re-run translation injection on the newly inserted elements
+        document.querySelectorAll('[data-translation]').forEach(item => {
+            const key = item.dataset.translation;
+            if (translations[key]) {
+                item.innerHTML = translations[key];
+            }
+        });
+
+        //const validLinks = links_translations[lang];
+
+        const validLinks = translations.links;
+
+        document.getElementById("bookPreviewFrame").setAttribute("src", `https://leer.amazon.es/kp/card?asin=${validLinks.book}&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_HJ6YDMXY6BRE1FA9AWE3`);
+        document.getElementById("preziPreviewFrame").setAttribute("src", `https://prezi.com/p/embed/${validLinks.prezi}`);
+
+        $('#mContactUs').on('show.bs.modal', function (e) {
+            console.log('opened');
+
+            const cuScriptExist = document.getElementById('cu_script');
+
+            if (!cuScriptExist) {
+                const script = document.createElement('script');
+                script.src = 'https://www.cognitoforms.com/f/seamless.js';
+                script.id = 'cu_script';
+                script.dataset.key = validLinks.contactUs.key;
+                script.dataset.form = validLinks.contactUs.form;
+                document.getElementById('divContactUs').appendChild(script);
+            }
+        });
+    })
+    .catch((e) => {
+        console.error(e);
+    });
